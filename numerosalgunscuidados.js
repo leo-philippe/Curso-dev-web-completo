84804e59ceb4b23558584a6e0259b3918d7655c1
Leonardo Philippe <leonardopsgomes@gmail.com>
2021-03-25 20:49:27 -0400

    Estudo sobre, cuidados com number, math e strings

---
+++ numerosalgunscuidados.js
@@ -0,0 +1,9 @@
+console.log(7/0) // Nesta situação, o valor retornado será "infinity"
+console.log("10"/2) /*Em js, caso o quando dividimos uma string por um numero,
+                      primeiramente ele vai verificar se a string é um numero, para ae sim,
+                      realizar a divisão, caso não seja reconhecido, retornará NaN "Not a Number"*/  
+
+console.log("show!"*2)
+console.log(0.1+0.7) // Nesta situação, ele não retornará o valor correto devido suas especificações
+//console.log(10.toString())
+console.log((10.345).toFixed(2))
